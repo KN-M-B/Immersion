e7b17c63edb76dd9a88778589f786f666380e4ff
KN-M-B <165977434+KN-M-B@users.noreply.github.com>
2024-04-11 15:56:07 +0800

    Update existing sites. Create site as practice for simple JS.

---
+++ KNMB/js/1.js
@@ -0,0 +1,12 @@
+function wipMessage() {
+  var var1 = 6;
+  var var2 = 8;
+  alert("Site under construction.");
+
+  console.log(
+    "           _ _          _____ ___  \r\n  _ __ ___ (_) | ___   _|___ // _ \\ \r\n | '_ ` _ \\| | |/ / | | | |_ \\ (_) |\r\n | | | | | | |   <| |_| |___) \\__, |\r\n |_| |_| |_|_|_|\\_\\\\__,_|____/  /_/ \r\n                                    "
+  );
+  console.log("Math.trunc(((var1 * 4) / 2 + (var2 + 10)) / 2 + 3.87298335 ** 2 + 9)  =  "+
+    Math.trunc(((var1 * 4) / 2 + (var2 + 10)) / 2 + 3.87298335 ** 2 + 9)
+  );
+}
